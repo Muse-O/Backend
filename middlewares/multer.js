a2df8a62eac40fc042ee270838aed8936bd692b2
dabeenkim <ekqls6812@gmail.com>
2023-04-05 21:08:06 +0900

    [fix]이미지작성#8

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,11 +5,12 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
-  filenameImage: function (req, file, cb) {
-    // cb(null, Date.now() + "-" + file.originalname);
-    cb(null, file.originalname + ".png");
+  filename: function (req, file, cb) {
+    const extension = file.originalname.split(".").pop();
+    cb(null, `${Date.now()}-${file.originalname}.${extension}`);
   },
 });
+
 const upload = multer({ storage: storage });
 
 module.exports = { upload };
