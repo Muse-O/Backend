28cc3d1d41aea41b21b95c562e3b2bf88735d760
WoogLim <elegy74@gmail.com>
2023-04-06 23:10:02 +0900

    [feat] 전시회 게시글 작성/수정 #25 #26

--- services/exhibition.service.js
+++ services/exhibition.service.js
@@ -13,7 +13,10 @@ class ExhibitionService {
    * @returns exhibitionItem
    */
   getExhibitionList = async (limit, offset) => {
-    const exhibitionItem = await this.exhibitionRepository.getExhibitionList(limit, offset);
+    const exhibitionItem = await this.exhibitionRepository.getExhibitionList(
+      limit,
+      offset
+    );
 
     if (!exhibitionItem.exhibitionList) {
       throw Boom.notFound("전시회 정보가 더 이상 없습니다.");
@@ -22,37 +25,100 @@ class ExhibitionService {
     return exhibitionItem;
   };
 
-  getExhibitionDetail = async (mode) => {
-    
+  getExhibitionDetail = async (mode) => {};
+
+  /**
+   * 전시회 작성/수정
+   * @param {string} mode 작성/수정 모드
+   * @param {string} userEmail 유저 이메일
+   * @param {exhibitionSchema} exhibitionReq 검증된 전시회 객체
+   * @returns 작성 결과 updateInfo
+   */
+  updateExhibition = async (mode, userEmail, exhibitionReq) => {
+    let updateInfo = {};
+
+    const {
+      artImage,
+      delImage,
+      exhibitionCategoty,
+      authors,
+      detailLocation,
+      ...exhibitionObj
+    } = exhibitionReq;
+
+    // 전시회 생성
+    const updateExhibitionItem =
+      await this.exhibitionRepository.updateExhibition(
+        mode,
+        userEmail,
+        exhibitionObj
+      );
+
+    if (!updateExhibitionItem) {
+      if (mode === "C")
+        throw new Boom.badRequest("전시회 게시글 작성에 실패했습니다.");
+      if (mode === "U")
+        throw new Boom.badRequest("전시회 게시글 수정에 실패했습니다.");
+    }
+
+    if (artImage.length > 0) {
+      // 이미지
+      const writeImageStatus =
+        await this.exhibitionRepository.updateExhibitionImg(
+          mode,
+          updateExhibitionItem.exhibitionId,
+          delImage,
+          artImage
+        );
+
+      // 이미지 수정 정보
+      updateInfo.imgUpdate = writeImageStatus;
+    }
+    if (exhibitionCategoty.length > 0) {
+      // 카테고리
+      const writeExhibitionCategories =
+        await this.exhibitionRepository.updateExhibitionCategory(
+          mode,
+          updateExhibitionItem.exhibitionId,
+          exhibitionCategoty
+        );
+
+      updateInfo.categoriesUpdate = writeExhibitionCategories;
+    }
+    if (authors.length > 0) {
+      // 작가
+      const writeAuthorsStatue =
+        await this.exhibitionRepository.updateExhibitionAuthors(
+          mode,
+          updateExhibitionItem.exhibitionId,
+          authors
+        );
+
+      updateInfo.authorUpdate = writeAuthorsStatue;
+    }
+    if (Object.keys(detailLocation).length > 0) {
+      // 장소
+      const writeExhibitionLocation =
+        await this.exhibitionRepository.updateExhibitionLocation(
+          mode,
+          updateExhibitionItem.exhibitionId,
+          detailLocation
+        );
+
+      updateInfo.locationUpdate = writeExhibitionLocation;
+    }
+
+    return updateInfo;
   };
 
-  writeExhibition = async (mode) => {
-    
-  };
+  deleteExhibition = async (mode) => {};
 
-  updateExhibition = async (mode) => {
-    
-  };
+  scrapExhibition = async (mode) => {};
 
-  deleteExhibition = async (mode) => {
-    
-  };
+  likeExhibition = async (mode) => {};
 
-  scrapExhibition = async (mode) => {
-    
-  };
+  searchExhibition = async (mode) => {};
 
-  likeExhibition = async (mode) => {
-    
-  };
-
-  searchExhibition = async (mode) => {
-    
-  };
-
-  searchCategoryExhibition = async (mode) => {
-    
-  };
-
+  searchCategoryExhibition = async (mode) => {};
 }
 module.exports = ExhibitionService;
