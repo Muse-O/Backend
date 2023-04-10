976a69222c4f3189a8f940391d66ee975f40a8ba
dabeenkim <ekqls6812@gmail.com>
2023-04-10 20:30:18 +0900

    [fix]아트그램 조회 작성 fix #8 #42

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const RedisConnector = require("../config/redisConnector");
+
+const redisClient = RedisConnector.getClient();
+
+redisClient.set("mykey", "myvalue", function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+redisClient.get("mykey", function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+redisClient.quit();
 
 module.exports = router;
