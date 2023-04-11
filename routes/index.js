91a75262b006262c8761b483d678c4827a0487b6
dabeenkim <ekqls6812@gmail.com>
2023-04-11 22:08:25 +0900

    [feat]검색기능, 자동완성틀잡기#120 #121

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const artgramRouter = require("./artgram.routes");
 const artgramCommentRouter = require("./artgramComment.routes");
 const exhibitionRouter = require("./exhibition.routes");
 const mypageRouter = require("./mypage.routes");
+const searchRouter = require("./search.routes");
 
 router.use("/auth", userRouter);
 router.use("/artgram", [artgramRouter, artgramCommentRouter]);
 router.use("/exhibition", exhibitionRouter);
-router.use("/mypage", mypageRouter)
+router.use("/mypage", mypageRouter);
+// router.use("/search", searchRouter);
 
 module.exports = router;
--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const SearchContorller = require("../controllers/search.controller");
-const searchController = new SearchContorller();
+// const SearchContorller = require("../controllers/search.controller");
+// const searchController = new SearchContorller();
 
-router.get("/", searchController.search);
+// /**
+//  * 검색기능
+//  */
+// router.get("/", searchController.search);
+// /**
+//  * 자동완성기능
+//  */
+// router.get("/auto", searchController.autocomplete);
 
-module.exports = router;
+// module.exports = router;
