0f6ea6c29943be9c05acf6c628476428dd2c4f61
dabeenkim <ekqls6812@gmail.com>
2023-04-12 20:00:07 +0900

    [fix]디자이너님 변경사항수정 아트그램조회#42

--- services/search.sevice.js
+++ services/search.sevice.js
@@ -1,23 +1,28 @@
 const SearchRepositroy = require("../repositories/search.repository");
-const Redis = require("ioredis");
-const redisClient = new Redis();
+// const Redis = require("ioredis");
+// const redisClient = new Redis({
+//   host: "ec2-43-201-154-28.ap-northeast-2.compute.amazonaws.com",
+//   port: 6379,
+// });
+const RedisConnector = require("../config/redisConnector");
 
 class SearchService {
   constructor() {
     this.searchRepositroy = new SearchRepositroy();
+    this.redisClient = RedisConnector.getClient();
   }
   /**
    * 검색기능
-   * @param {query} searchText
+   * @param {query} keyWord
    * @returns
    */
-  search = async (searchText) => {
+  search = async (keyWord) => {
     //redis 캐시에서 검색 결과 확인
-    const cachedArtgramTitles = await redisClient.get(
-      `search:artgram:${searchText}`
+    const cachedArtgramTitles = await this.redisClient.get(
+      `search:artgram:${keyWord}`
     );
-    const cachedExhibitionTitles = await redisClient.get(
-      `search:exhibition:${searchText}`
+    const cachedExhibitionTitles = await this.redisClient.get(
+      `search:exhibition:${keyWord}`
     );
 
     let artgramTitles, exhibitionTitles;
@@ -27,11 +32,11 @@ class SearchService {
       artgramTitles = JSON.parse(cachedArtgramTitles);
     } else {
       //캐시되지 않은 경우 Artgrams테이블에서 해당 값을 가져오기
-      artgramTitles = await this.SearchRepositroy.searchArtgrams(searchText);
+      artgramTitles = await this.searchRepositroy.searchArtgrams(keyWord);
       artgramTitles = artgramTitles.map((row) => row.artgramTitle);
       //다음에 검색할땐 빠르게 검색하기 위해 redis에 제목을 캐시
       await this.redisClient.set(
-        `search:artgram:${searchText}`,
+        `search:artgram:${keyWord}`,
         JSON.stringify(artgramTitles),
         "EX",
         3600
@@ -43,13 +48,11 @@ class SearchService {
       exhibitionTitles = JSON.parse(cachedExhibitionTitles);
     } else {
       //캐시되지 않은 경우 Exhibitions테이블에서 해당 값을 가져오기
-      exhibitionTitles = await this.searchRepositroy.searchExhibition(
-        searchText
-      );
+      exhibitionTitles = await this.searchRepositroy.searchExhibition(keyWord);
       exhibitionTitles = exhibitionTitles.map((row) => row.exhibitionTitle);
       //다음에 검색할땐 빠르게 검색하기 위해 redis에 제목을 캐시
       await this.redisClient.set(
-        `search:${searchText}`,
+        `search:${keyWord}`,
         JSON.stringify(exhibitionTitles),
         "EX",
         3600
@@ -64,15 +67,15 @@ class SearchService {
 
   /**
    * 검색어 자동완성
-   * @param {query} searchText
+   * @param {query} keyWord
    * @returns
    */
-  autocomplete = async (searchText) => {
+  autocomplete = async (keyWord) => {
     const artgramSuggestions = await this.searchRepositroy.autocompleteArtgrams(
-      searchText
+      keyWord
     );
     const exhibitionSuggestions =
-      await this.searchRepositroy.autocompleteExhibition(searchText);
+      await this.searchRepositroy.autocompleteExhibition(keyWord);
 
     return {
       artgramTitles: artgramSuggestions,
