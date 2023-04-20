e977abdfce88a4fed4acd315dc40171138a515b4
mseoa <0527sophie@naver.com>
2023-04-20 12:12:33 +0900

    [style]mypage layer 프리티어 적용,jsdocs 추가

--- repositories/mypage.repository.js
+++ repositories/mypage.repository.js
@@ -1,257 +1,339 @@
-const { UserProfile, Exhibitions, ExhibitionLike, ExhibitionScrap, Artgrams, ArtgramImg, Users, ArtgramHashtag, ArtgramLike, ArtgramScrap, ArtgramsComment, sequelize } = require('../models');
-const { parseModelToFlatObject } = require('../modules/parseModelToFlatObject')
-const { Op } = require('sequelize');
+const {
+  UserProfile,
+  Exhibitions,
+  ExhibitionLike,
+  ExhibitionScrap,
+  Artgrams,
+  ArtgramImg,
+  ArtgramLike,
+  ArtgramScrap,
+  sequelize,
+} = require("../models");
+const { parseModelToFlatObject } = require("../modules/parseModelToFlatObject");
+const { Op } = require("sequelize");
 
-class MypageRepository{
-    findProfileByEmail = async (userEmail) => {
-        const profile = await UserProfile.findOne({
-            attributes: ['profileImg','profileNickname','profileIntro'],
-            where: [{user_email:userEmail}]
-        })
-        return profile;
+class MypageRepository {
+  /**
+   *이메일과 일치하는 프로필 조회
+   * @param {string} userEmail
+   * @returns 프로필
+   */
+  findProfileByEmail = async (userEmail) => {
+    const profile = await UserProfile.findOne({
+      attributes: ["profileImg", "profileNickname", "profileIntro"],
+      where: [{ user_email: userEmail }],
+    });
+    return profile;
+  };
+
+  /**
+   *프로필 수정
+   * @param {string} profileImg
+   * @param {string} nickname
+   * @param {string} introduction
+   * @param {string} userEmail
+   * @returns 수정된 프로필
+   */
+  updateMyProfile = async (profileImg, nickname, introduction, userEmail) => {
+    const result = await UserProfile.update(
+      {
+        profileImg: profileImg,
+        profileNickname: nickname,
+        profileIntro: introduction,
+      },
+      { where: { user_email: userEmail } }
+    );
+
+    const updatedProfile = await this.findProfileByEmail(userEmail);
+    return updatedProfile;
+  };
+
+  /**
+   *내가 작성한 전시회 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns myExhibitionList, paginationInfo
+   */
+  findMyPostExhibition = async (limit, offset, userEmail) => {
+    const myExhibition = await Exhibitions.findAll({
+      attributes: ["exhibition_id", "exhibition_title", "post_image"],
+      where: [
+        { user_email: userEmail, exhibition_status: { [Op.ne]: "ES04" } },
+      ],
+      order: [["created_at", "DESC"]],
+      raw: true,
+      limit: limit,
+      offset: offset,
+      subQuery: false,
+    }).then((models) => models.map(parseModelToFlatObject));
+
+    const myExhibitionList = {
+      result: myExhibition,
     };
 
-    updateMyProfile = async (profileImg, nickname, introduction, userEmail) =>{
-        const result = await UserProfile.update({ 
-            profileImg : profileImg,
-            profileNickname: nickname,
-            profileIntro: introduction
-         }, { where: {user_email:userEmail} });
+    const myExhibitionCnt = await Exhibitions.count({
+      where: {
+        user_email: userEmail,
+        exhibition_status: {
+          [Op.ne]: "ES04",
+        },
+      },
+    });
 
-        const updatedProfile = await this.findProfileByEmail(userEmail);
-        return updatedProfile;
+    const hasNextPage = offset + limit < myExhibitionCnt;
+
+    const paginationInfo = {
+      limit,
+      offset,
+      myExhibitionCnt,
+      hasNextPage,
     };
 
-    findMyPostExhibition = async (limit, offset, userEmail) => {
+    return { myExhibitionList, paginationInfo };
+  };
 
-        const myExhibition = await Exhibitions.findAll({
-            attributes: ['exhibition_id','exhibition_title', 'post_image'],
-            where: [{user_email: userEmail, exhibition_status: {[Op.ne]:"ES04"}}],
-            order: [['created_at', 'DESC']],
-            raw:true,
-            limit: limit,
-            offset: offset,
-            subQuery: false,
-        }).then((models) => models.map(parseModelToFlatObject));
+  /**
+   *내가 좋아요한 전시 게시글 조회
+   * @param {string} userEmail
+   * @returns 좋아요한 전시게시글 아이디: array
+   */
+  findAllMyLikedExhibitionId = async (userEmail) => {
+    const myLikes = await ExhibitionLike.findAll({
+      attributes: ["exhibition_id"],
+      where: [{ user_email: userEmail }],
+      order: [["created_at", "DESC"]],
+      raw: true,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-        const myExhibitionList = {
-            result: myExhibition
-        }
+    return myLikes;
+  };
 
-        const myExhibitionCnt = await Exhibitions.count({
-            where:{
-                user_email: userEmail,
-                exhibition_status: {
-                    [Op.ne]: "ES04",
-                  },
-            }
-        })
+  /**
+   *내가 좋아요/스크랩한 전시게시글 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {array} myLikedExhibitionIds myScrappedExhibitionIds
+   * @returns exhibitionList, paginationInfo
+   */
+  findMyExhibition = async (limit, offset, myLikedExhibitionIds) => {
+    const myExhibitions = await Exhibitions.findAll({
+      attributes: ["exhibition_id", "exhibition_title", "post_image"],
+      raw: true,
+      where: {
+        exhibition_id: {
+          [Op.in]: myLikedExhibitionIds,
+        },
+        exhibition_status: { [Op.ne]: "ES04" },
+      },
+      order: [
+        [
+          sequelize.literal(
+            `FIELD(exhibition_id, ${myLikedExhibitionIds
+              .map((id) => `'${id}'`)
+              .join(",")})`
+          ),
+        ],
+      ],
+      limit: limit,
+      offset: offset,
+      subQuery: false,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-        const hasNextPage = offset + limit < myExhibitionCnt;
+    const exhibitionList = {
+      result: myExhibitions,
+    };
 
-        const paginationInfo = {
-            limit,
-            offset,
-            myExhibitionCnt,
-            hasNextPage
-        }
+    const myExhibitionCnt = await Exhibitions.count({
+      where: {
+        exhibition_id: {
+          [Op.in]: myLikedExhibitionIds,
+        },
+        exhibition_status: {
+          [Op.ne]: "ES04",
+        },
+      },
+    });
 
-        return {myExhibitionList, paginationInfo}
-    }
+    const hasNextPage = offset + limit < myExhibitionCnt;
 
-    findAllMyLikedExhibitionId = async (userEmail) => {
-        const myLikes = await ExhibitionLike.findAll({
-            attributes: ['exhibition_id'],
-            where:[{user_email: userEmail}],
-            order: [['created_at', 'DESC']],
-            raw: true,
-        }).then((models) => models.map(parseModelToFlatObject));
+    const paginationInfo = {
+      limit,
+      offset,
+      myExhibitionCnt,
+      hasNextPage,
+    };
 
-        return myLikes
-    }
+    return { exhibitionList, paginationInfo };
+  };
 
+  /**
+   *내가 스크랩한 전시 게시글 조회
+   * @param {string} userEmail
+   * @returns 스크랩한 전시게시글 아이디: array
+   */
+  findAllMyScrappedExhibitionId = async (userEmail) => {
+    const myScraps = await ExhibitionScrap.findAll({
+      attributes: ["exhibition_id"],
+      where: [{ user_email: userEmail }],
+      order: [["created_at", "DESC"]],
+      raw: true,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-    findMyExhibition = async (limit, offset, myLikedExhibitionIds) => {
-        const myExhibitions = await Exhibitions.findAll({
-            attributes: ['exhibition_id','exhibition_title', 'post_image'],
-            raw: true,
-            where: {
-                exhibition_id : {
-                    [Op.in]: myLikedExhibitionIds
-                },
-                exhibition_status: {[Op.ne]: "ES04"}
-            },
-            order: [
-                [sequelize.literal(`FIELD(exhibition_id, ${myLikedExhibitionIds.map(id => `'${id}'`).join(',')})`)]
-            ],
-            limit: limit,
-            offset: offset,
-            subQuery: false,
-        }).then((models) => models.map(parseModelToFlatObject));
+    return myScraps;
+  };
 
-        const exhibitionList = {
-            result: myExhibitions
-        }
+  /**
+   *내가 작성한 아트그램 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns myArtgramList, paginationInfo
+   */
+  findMyPostArtgram = async (limit, offset, userEmail) => {
+    const myArtgram = await Artgrams.findAll({
+      attributes: ["artgram_id", "artgram_title"],
+      include: [
+        {
+          model: ArtgramImg,
+          attributes: ["imgUrl"],
+          where: { imgOrder: 1 },
+          seperate: true,
+        },
+      ],
+      where: {
+        userEmail: userEmail,
+        artgram_status: {
+          [Op.ne]: "AS04",
+        },
+      },
+      order: [["createdAt", "DESC"]],
+      raw: true,
+      limit: limit,
+      offset: offset,
+      subQuery: false,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-        const myExhibitionCnt = await Exhibitions.count({
-            where:{
-                exhibition_id : {
-                    [Op.in]: myLikedExhibitionIds
-                },
-                exhibition_status: {
-                    [Op.ne]: "ES04",
-                  },
-            }
-        })
+    const myArtgramList = {
+      result: myArtgram,
+    };
 
-        const hasNextPage = offset + limit < myExhibitionCnt;
+    const myArtgramCnt = await Artgrams.count({
+      where: {
+        userEmail: userEmail,
+        artgram_status: {
+          [Op.ne]: "AS04",
+        },
+      },
+    });
 
-        const paginationInfo = {
-            limit,
-            offset,
-            myExhibitionCnt,
-            hasNextPage
-        }
+    const hasNextPage = offset + limit < myArtgramCnt;
 
-        return {exhibitionList, paginationInfo}
-    }
+    const paginationInfo = {
+      limit,
+      offset,
+      myArtgramCnt,
+      hasNextPage,
+    };
 
-    findAllMyScrappedExhibitionId = async (userEmail) => {
-        const myScraps = await ExhibitionScrap.findAll({
-            attributes: ['exhibition_id'],
-            where:[{user_email: userEmail}],
-            order: [['created_at', 'DESC']],
-            raw: true,
-        }).then((models) => models.map(parseModelToFlatObject));
+    return { myArtgramList, paginationInfo };
+  };
 
-        return myScraps
-    }
+  /**
+   *내가 좋아요한 아트그램 아이디들 조회
+   * @param {string} userEmail
+   * @returns 좋아요한 아트그램 아이디: array
+   */
+  findAllMyLikedArtgramId = async (userEmail) => {
+    const myLikes = await ArtgramLike.findAll({
+      attributes: ["artgram_id"],
+      where: [{ user_email: userEmail }],
+      order: [["created_at", "DESC"]],
+      raw: true,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-    findMyPostArtgram = async (limit,offset,userEmail) => {
-        const myArtgram = await Artgrams.findAll({
-            attributes: ['artgram_id','artgram_title'],
-            include: [
-                {
-                  model: ArtgramImg,
-                  attributes: ["imgUrl"],
-                  where: {"imgOrder":1},
-                  seperate: true
-                },
-              ],
-            where: {
-                userEmail: userEmail,
-                artgram_status: {
-                  [Op.ne]: "AS04",
-                },
-              },
-            order: [["createdAt", "DESC"]], 
-            raw: true,
-            limit: limit,
-            offset: offset,
-            subQuery: false
-        }).then((models) => models.map(parseModelToFlatObject))
+    return myLikes;
+  };
 
-        const myArtgramList = {
-            result: myArtgram
-        }
+  /**
+   *내 좋아요/스크랩한 아트그램 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {array} myLikedArtgramIds myScrappedArtgramIds
+   * @returns artgramList, paginationInfo
+   */
+  findMyArtgram = async (limit, offset, myLikedArtgramIds) => {
+    const myArtgrams = await Artgrams.findAll({
+      attributes: ["artgram_id", "artgram_title"],
+      raw: true,
+      include: [
+        {
+          model: ArtgramImg,
+          attributes: ["imgUrl"],
+          where: { imgOrder: 1 },
+          seperate: true,
+        },
+      ],
+      where: {
+        artgram_id: {
+          [Op.in]: myLikedArtgramIds,
+        },
+        artgram_status: {
+          [Op.ne]: "AS04",
+        },
+      },
+      order: sequelize.literal(
+        `FIELD(Artgrams.artgram_id, ${myLikedArtgramIds
+          .map((id) => `'${id}'`)
+          .join(",")})`
+      ),
+      limit: limit,
+      offset: offset,
+      subQuery: false,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-        const myArtgramCnt = await Artgrams.count({
-            where:{
-                userEmail: userEmail,
-                artgram_status: {
-                    [Op.ne]: "AS04",
-                  },
-            }
-        })
+    const artgramList = {
+      result: myArtgrams,
+    };
 
-        const hasNextPage = offset + limit < myArtgramCnt;
+    const myArtgramCnt = await Artgrams.count({
+      where: {
+        artgram_id: {
+          [Op.in]: myLikedArtgramIds,
+        },
+        artgram_status: {
+          [Op.ne]: "AS04",
+        },
+      },
+    });
 
-        const paginationInfo = {
-            limit,
-            offset,
-            myArtgramCnt,
-            hasNextPage
-        }
+    const hasNextPage = offset + limit < myArtgramCnt;
 
-        return {myArtgramList, paginationInfo}
-    }
+    const paginationInfo = {
+      limit,
+      offset,
+      myArtgramCnt,
+      hasNextPage,
+    };
 
-    findAllMyLikedArtgramId = async (userEmail) => {
-        const myLikes = await ArtgramLike.findAll({
-            attributes: ['artgram_id'],
-            where:[{user_email: userEmail}],
-            order: [['created_at', 'DESC']],
-            raw: true,
-        }).then((models) => models.map(parseModelToFlatObject));
+    return { artgramList, paginationInfo };
+  };
 
-        return myLikes
-    }
+  /**
+   *내가 스크랩한 아트그램 아이디들 조회
+   * @param {string} userEmail
+   * @returns 스크랩한 아트그램 아이디: array
+   */
+  findAllMyScrappedArtgramId = async (userEmail) => {
+    const myLikes = await ArtgramScrap.findAll({
+      attributes: ["artgram_id"],
+      where: [{ user_email: userEmail }],
+      order: [["created_at", "DESC"]],
+      raw: true,
+    }).then((models) => models.map(parseModelToFlatObject));
 
-    findMyArtgram = async (limit, offset, myLikedArtgramIds) => {
-        const myArtgrams = await Artgrams.findAll({
-            attributes: ['artgram_id','artgram_title'],
-            raw: true,
-            include: [
-                {
-                  model: ArtgramImg,
-                  attributes: ["imgUrl"],
-                  where: {"imgOrder":1},
-                  seperate: true
-                },
-              ],
-            where: {
-                artgram_id : {
-                    [Op.in]: myLikedArtgramIds
-                },
-                artgram_status: {
-                  [Op.ne]: "AS04",
-                },
-              },
-            order: 
-            sequelize.literal(`FIELD(Artgrams.artgram_id, ${myLikedArtgramIds.map(id => `'${id}'`).join(',')})`)
-            ,
-            limit: limit,
-            offset: offset,
-            subQuery: false,
-        }).then((models) => models.map(parseModelToFlatObject))
-
-        const artgramList = {
-            result: myArtgrams
-        }
-
-        const myArtgramCnt = await Artgrams.count({
-            where:{
-                artgram_id : {
-                    [Op.in]: myLikedArtgramIds
-                },
-                artgram_status: {
-                    [Op.ne]: "AS04",
-                  },
-            }
-        })
-
-        const hasNextPage = offset + limit < myArtgramCnt;
-
-        const paginationInfo = {
-            limit,
-            offset,
-            myArtgramCnt,
-            hasNextPage
-        }
-
-        return {artgramList, paginationInfo}
-    }
-
-    findAllMyScrappedArtgramId = async (userEmail) => {
-        const myLikes = await ArtgramScrap.findAll({
-            attributes: ['artgram_id'],
-            where:[{user_email: userEmail}],
-            order: [['created_at', 'DESC']],
-            raw: true,
-        }).then((models) => models.map(parseModelToFlatObject));
-
-        return myLikes
-    }
+    return myLikes;
+  };
 }
 
 module.exports = MypageRepository;
--- services/mypage.service.js
+++ services/mypage.service.js
@@ -1,90 +1,182 @@
-const MypageRepository = require('../repositories/mypage.repository');
+const MypageRepository = require("../repositories/mypage.repository");
 const Boom = require("boom");
-const { use } = require('passport');
 
 class MypageService {
-    mypageRepository = new MypageRepository();
+  mypageRepository = new MypageRepository();
 
-    getMyProfile = async (userEmail) => {
-        const profile = await this.mypageRepository.findProfileByEmail(userEmail);
-        if (!profile){
-            throw Boom.notFound("서버 측 오류로 프로필이 존재하지 않습니다.");
-        }
-        return profile;
+  /**
+   * 내 프로필 조회
+   * @param {string} userEmail
+   * @returns 프로필
+   */
+  getMyProfile = async (userEmail) => {
+    const profile = await this.mypageRepository.findProfileByEmail(userEmail);
+    if (!profile) {
+      throw Boom.notFound("서버 측 오류로 프로필이 존재하지 않습니다.");
+    }
+    return profile;
+  };
+
+  /**
+   * 내 프로필 수정
+   * @param {string} profileImg
+   * @param {string} nickname
+   * @param {string} introduction
+   * @param {string} userEmail
+   * @returns 수정된 프로필
+   */
+  updateMyProfile = async (profileImg, nickname, introduction, userEmail) => {
+    const profile = await this.mypageRepository.findProfileByEmail(userEmail);
+    if (!profile) {
+      throw Boom.notFound("서버 측 오류로 프로필이 존재하지 않습니다.");
+    }
+    const updatedProfile = await this.mypageRepository.updateMyProfile(
+      profileImg,
+      nickname,
+      introduction,
+      userEmail
+    );
+    return updatedProfile;
+  };
+
+  /**
+   * 내가 작성한 전시회 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 작성한 전시회
+   */
+  getMyExhibition = async (limit, offset, userEmail) => {
+    const exhibitions = await this.mypageRepository.findMyPostExhibition(
+      limit,
+      offset,
+      userEmail
+    );
+
+    return exhibitions;
+  };
+
+  /**
+   * 내가 좋아요한 전시회 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 좋아요한 전시회
+   */
+  getMyLikedExhibition = async (limit, offset, userEmail) => {
+    const myLikes = await this.mypageRepository.findAllMyLikedExhibitionId(
+      userEmail
+    );
+
+    if (!myLikes.length) {
+      return { message: "좋아요한 게시글이 없습니다." };
     }
 
-    updateMyProfile = async (profileImg, nickname, introduction, userEmail) => {
-        const profile = await this.mypageRepository.findProfileByEmail(userEmail);
-        if (!profile){
-            throw Boom.notFound("서버 측 오류로 프로필이 존재하지 않습니다.");
-        }
-        const updatedProfile = await this.mypageRepository.updateMyProfile(profileImg, nickname, introduction, userEmail);
-        return updatedProfile;
+    const myLikedExhibitionIds = myLikes.map((elem) => elem.exhibition_id);
+    const getMyLikedExhibitions = await this.mypageRepository.findMyExhibition(
+      limit,
+      offset,
+      myLikedExhibitionIds
+    );
+
+    return getMyLikedExhibitions;
+  };
+
+  /**
+   * 내가 스크랩한 전시회 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 스크랩한 전시회
+   */
+  getMyScrappedExhibition = async (limit, offset, userEmail) => {
+    const myScraps = await this.mypageRepository.findAllMyScrappedExhibitionId(
+      userEmail
+    );
+
+    if (!myScraps.length) {
+      return { message: "스크랩한 게시글이 없습니다." };
     }
 
-    getMyExhibition = async (limit, offset, userEmail) => {
-        const exhibitions = await this.mypageRepository.findMyPostExhibition(limit, offset, userEmail);
-        
-        return exhibitions
-    };
+    const myScrappedExhibitionIds = myScraps.map((elem) => elem.exhibition_id);
 
-    getMyLikedExhibition = async (limit, offset, userEmail) =>{
-        const myLikes = await this.mypageRepository.findAllMyLikedExhibitionId(userEmail);
-        
-        if(!myLikes.length){
-            return {"message": "좋아요한 게시글이 없습니다."}
-        }
+    const getMyScrappedExhibitions =
+      await this.mypageRepository.findMyExhibition(
+        limit,
+        offset,
+        myScrappedExhibitionIds
+      );
+    return getMyScrappedExhibitions;
+  };
 
-        const myLikedExhibitionIds = myLikes.map((elem)=>elem.exhibition_id);
-        const getMyLikedExhibitions = await this.mypageRepository.findMyExhibition(limit, offset, myLikedExhibitionIds)
-        
-        return getMyLikedExhibitions
+  /**
+   * 내가 작성한 아트그램 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 작성한 아트그램
+   */
+  getMyArtgram = async (limit, offset, userEmail) => {
+    const artgrams = await this.mypageRepository.findMyPostArtgram(
+      limit,
+      offset,
+      userEmail
+    );
+
+    return artgrams;
+  };
+
+  /**
+   * 내가 좋아요한 아트그램 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 좋아요한 아트그램
+   */
+  getMyLikedArtgram = async (limit, offset, userEmail) => {
+    const myLikes = await this.mypageRepository.findAllMyLikedArtgramId(
+      userEmail
+    );
+
+    if (!myLikes.length) {
+      return { message: "좋아요한 게시글이 없습니다." };
     }
 
-    getMyScrappedExhibition= async (limit, offset, userEmail) => {
-        const myScraps = await this.mypageRepository.findAllMyScrappedExhibitionId(userEmail);
+    const myLikedArtgramIds = myLikes.map((elem) => elem.artgram_id);
+    const getMyLikedExhibitions = await this.mypageRepository.findMyArtgram(
+      limit,
+      offset,
+      myLikedArtgramIds
+    );
 
-        if(!myScraps.length){
-            return {"message": "스크랩한 게시글이 없습니다."}
-        }
+    return getMyLikedExhibitions;
+  };
 
-        const myScrappedExhibitionIds = myScraps.map((elem)=>elem.exhibition_id);
+  /**
+   * 내가 스크랩한 아트그램 조회
+   * @param {number} limit
+   * @param {number} offset
+   * @param {string} userEmail
+   * @returns 내가 스크랩한 아트그램
+   */
+  getMyScrappedArtgram = async (limit, offset, userEmail) => {
+    const myScraps = await this.mypageRepository.findAllMyScrappedArtgramId(
+      userEmail
+    );
 
-        const getMyScrappedExhibitions = await this.mypageRepository.findMyExhibition(limit, offset, myScrappedExhibitionIds);
-        return getMyScrappedExhibitions
+    if (!myScraps.length) {
+      return { message: "스크랩한 게시글이 없습니다." };
     }
 
-    getMyArtgram = async (limit, offset, userEmail) => {
-        const artgrams = await this.mypageRepository.findMyPostArtgram(limit, offset, userEmail);
-        
-        return artgrams
-    }
+    const myScrappedArtgramIds = myScraps.map((elem) => elem.artgram_id);
+    const getMyScrappedArtgrams = await this.mypageRepository.findMyArtgram(
+      limit,
+      offset,
+      myScrappedArtgramIds
+    );
 
-    getMyLikedArtgram = async (limit, offset, userEmail) => {
-        const myLikes = await this.mypageRepository.findAllMyLikedArtgramId(userEmail);
-        
-        if(!myLikes.length){
-            return {"message": "좋아요한 게시글이 없습니다."}
-        }
-
-        const myLikedArtgramIds = myLikes.map((elem)=>elem.artgram_id);
-        const getMyLikedExhibitions = await this.mypageRepository.findMyArtgram(limit, offset, myLikedArtgramIds)
-        
-        return getMyLikedExhibitions
-    }
-
-    getMyScrappedArtgram = async (limit, offset, userEmail) => {
-        const myScraps = await this.mypageRepository.findAllMyScrappedArtgramId(userEmail);
-        
-        if(!myScraps.length){
-            return {"message": "스크랩한 게시글이 없습니다."}
-        }
-
-        const myScrappedArtgramIds = myScraps.map((elem)=>elem.artgram_id);
-        const getMyScrappedArtgrams = await this.mypageRepository.findMyArtgram(limit, offset, myScrappedArtgramIds)
-        
-        return getMyScrappedArtgrams
-    }
+    return getMyScrappedArtgrams;
+  };
 }
 
 module.exports = MypageService;
