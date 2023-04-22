dae71bcee13e036ed5d724cc340d1bf7623b97d5
dabeenkim <ekqls6812@gmail.com>
2023-04-22 21:58:43 +0900

    [fix]어드민 라우터 이름오타수정

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -4,4 +4,4 @@ const router = express.Router();
 const ArtgramController = require("../controllers/artgram.controller");
 const adminController = new ArtgramController();
 
-module.exports = reuter;
+module.exports = router;
