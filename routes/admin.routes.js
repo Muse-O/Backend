787190136c6f1684dc67ec2185eb9fb36832e8d8
dabeenkim <ekqls6812@gmail.com>
2023-04-26 00:37:49 +0900

    [fix]아트그램검색부분 수정

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware");
+// const express = require("express");
+// const router = express.Router();
+// const authMiddleware = require("../middlewares/authMiddleware");
 
-const AdminController = require("../controllers/admin.controller");
-const adminController = new AdminController();
+// const AdminController = require("../controllers/admin.controller");
+// const adminController = new AdminController();
 
-router.get("/admin/approvalList", adminController.getPendingExhibitions);
-router.patch("/admin/approvalList", adminController.approveExhibition);
-router.get("/admin/reportList", adminController.getAllReports);
-router.patch("/admin/reportList", adminController.processReport);
+// router.get("/admin/approvalList", adminController.getPendingExhibitions);
+// router.patch("/admin/approvalList", adminController.approveExhibition);
+// router.get("/admin/reportList", adminController.getAllReports);
+// router.patch("/admin/reportList", adminController.processReport);
 
-module.exports = router;
+// module.exports = router;
