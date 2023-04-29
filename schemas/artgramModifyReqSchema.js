cdc2720832e442887ae70a13b248e7a1078d41fa
dabeenkim <ekqls6812@gmail.com>
2023-04-29 22:16:43 +0900

    [fix]아트그램 수정부분 타이틀,내용 undifined허용#9

--- schemas/artgramModifyReqSchema.js
+++ schemas/artgramModifyReqSchema.js
@@ -1,15 +1,11 @@
 const Joi = require("joi");
 
 const artgramModify = Joi.object({
-  artgramTitle: Joi.string().required().messages({
+  artgramTitle: Joi.string().messages({
     "string.empty": "artgramTitle (제목)을 문자열로 입력하세요",
-    "any.required":
-      "artgramTitle (제목) 값이 요청 매개 변수로 전달되지 않았습니다",
   }),
-  artgramDesc: Joi.string().required().messages({
+  artgramDesc: Joi.string().messages({
     "string.empty": "artgramDesc (설명)을 문자열로 입력하세요",
-    "any.required":
-      "artgramDesc (설명) 값이 요청 매개 변수로 전달되지 않았습니다.",
   }),
   hashtag: Joi.alternatives()
     .try(Joi.string(), Joi.array().items(Joi.string()))
